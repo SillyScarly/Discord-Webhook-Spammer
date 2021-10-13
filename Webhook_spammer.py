2f2c0c492c35d72f08b0147c59b40bd614565a28
ZeroDay3791 <86209745+ZeroDay3791@users.noreply.github.com>
2021-10-12 22:19:44 -0400

    Create Webhook_spammer.py

---
+++ Webhook_spammer.py
@@ -0,0 +1,21 @@
+```python
+
+import requests
+
+url = "webhook url"
+
+data = {
+    "content" : "Message",
+    "username" : "GO FUCK YOURSELFS"
+}
+
+while True:
+    result = requests.post(url, json = data)
+    try:
+      result.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+      print(err)
+    else:
+      print("Payload delivered successfully, code {}.".format(result.status_code))
+
+```
