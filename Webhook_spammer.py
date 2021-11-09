2f11fcec886afdbaad7cfaaef8cdaef1a4c90d08
ZeroDay3791 <86209745+ZeroDay3791@users.noreply.github.com>
2021-11-09 04:59:35 +0000

    Update Webhook_spammer.py

--- Webhook_spammer.py
+++ Webhook_spammer.py
@@ -1,18 +1,18 @@
 import requests
 
-url = "webhook url"
+url = "https://discord.com/api/webhooks/904856971683963030/9jqp7ah8rGYY_GqKtGdNZ6-oTi5JoHqe9agKwnuovMkqwgC69_AFr1UEZBL2Puu4NlEw"
+
 
 data = {
-    "content" : "Message",
-    "username" : "Raid Bot"
+    "content": "@everyone get raided",
+    "username": "Raid Bot"
 }
 
 while True:
-    result = requests.post(url, json = data)
+    result = requests.post(url, json=data)
     try:
-      result.raise_for_status()
+        result.raise_for_status()
     except requests.exceptions.HTTPError as err:
-      print(err)
+        print(err)
     else:
-      print("Payload delivered successfully, code {}.".format(result.status_code))
-
+        print("Payload delivered successfully, code {}.".format(result.status_code))
